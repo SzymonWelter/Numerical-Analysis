5abfbb92c213e5dfcda7a011195caa237ef562f6
SzymonWelter <szymon.welter98@gmail.com>
2019-03-12 13:31:16 +0100

    reset

--- Interpolations/chebNodes.m
+++
@@ -1,6 +0,0 @@
-function xn = chebNodes(first, last, nodeNum)
-    xn = zeros(1, nodeNum);
-    for i = 1 : nodeNum
-        xn(i) = ((first+last) + (last-first)*cos((2*i - 1)*pi/(2*nodeNum)))/2;
-    end   
-end
--- Interpolations/newton.m
+++
@@ -1,9 +0,0 @@
-function N = newton(xs, ys, x, divDiff)
-    N = ys(1);
-    [~,k] = size(xs);
-    w = 1;
-    for j = 2 : k
-        w = w * (x - xs(j-1));  
-        N = w * divDiff(j)+ N;
-    end
-end
