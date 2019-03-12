ac7b3406c18027926696bdce14d7b55dde254f13
SzymonWelter <szymon.welter98@gmail.com>
2019-03-12 14:15:23 +0100

    reset

--- Interpolations/dividedDifferences.m
+++
@@ -1,9 +0,0 @@
-function a = dividedDifferences( xn, yn )
-    a = yn;
-    [~,s] = size(yn);
-    for i = 1 : s 
-        for j = s : -1 : i+1
-            a(j)=(a(j)-a(j-1))/(xn(j)-xn(j-i));
-        end
-    end
-end
