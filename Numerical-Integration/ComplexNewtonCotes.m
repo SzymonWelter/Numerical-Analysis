a5d59f3ee34d4e0e0731ba81d2493f39d4b96abb
SzymonWelter <szymon.welter98@gmail.com>
2019-05-18 20:14:09 +0200

    last range simplified and added results to report

--- Numerical-Integration/ComplexNewtonCotes.m
+++ Numerical-Integration/ComplexNewtonCotes.m
@@ -1,12 +1,12 @@
 function result = ComplexNewtonCotes(fi,h,d)
     result = 0;
     n = length(fi);
-    last = mod(n-1, d) + d;
-    for i = 1 : d : n - last
+    last = mod(n-1, d);
+    for i = 1 : d : n - last - d
         result = result + SimpleNewtonCotes(fi(i : i+d), h);
     end
-    if last ~= d
-        result = result + SimpleNewtonCotes(fi(n-last+d : n), h);
+    if last ~= 0
+        result = result + SimpleNewtonCotes(fi(n-last : n), h);
     end
     
 end
